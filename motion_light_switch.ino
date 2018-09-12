f3d9f603af777ccef366af71ad7d9cecef410fd3
Jānis Šlapiņš <janis_slapins@inbox.lv>
2018-09-12 09:49:24 +0300

    Move the condition checking loop to the loop() function, remove unnecessary variables

--- motion_light_switch.ino
+++ motion_light_switch.ino
@@ -10,10 +10,7 @@ const int pinMotionSensor = 2;
 const int pinLightSensor = A0;
 
 int stateLightSwitch = LOW;
-
-boolean motionDetected;
 uint32_t lastMotionTime;
-
 
 /*
  * Light intensity ranges:
@@ -33,43 +30,38 @@ void setup() {
   pinMode(pinLightSwitch, OUTPUT);
   pinMode(pinMotionSensor, INPUT);
   pinMode(pinLightSensor, INPUT);
+  lastMotionTime = millis();
 }
 
 void loop() {
   int curLightIntensity = analogRead(pinLightSensor);
-  motionDetected = (boolean)digitalRead(pinMotionSensor);
+  uint32_t curTime = millis();
 
-  if (motionDetected) {
+  if ((boolean)digitalRead(pinMotionSensor)) {
     lastMotionTime = millis();
   }
 
-  if (motionDetected && curLightIntensity < thresholdLightIntensity) {
-    switch_on();
+  while (curLightIntensity < thresholdLightIntensity && curTime - lastMotionTime < lengthLightOn) {
+     switch_on();
+
+     curLightIntensity = analogRead(pinLightSensor);
+
+     if ((boolean)digitalRead(pinMotionSensor)) {
+      lastMotionTime = millis();
+     }
+
+     curTime = millis();
   }
-  else {
-    switch_off();
-  }
+
+  switch_off();
 }
 
-// Switch on the light for a preset duration
-// and check again for a motion after that 
+// Switch on the light if it is off 
 void switch_on() {
-  uint32_t curTime = millis();
-
-  while (curTime - lastMotionTime < lengthLightOn) {
     if (stateLightSwitch == LOW) {
       stateLightSwitch = HIGH;
       digitalWrite(pinLightSwitch, stateLightSwitch);
     }
-
-    motionDetected = (boolean)digitalRead(pinMotionSensor);
-
-    if (motionDetected) {
-      lastMotionTime = millis();
-    }
-
-    curTime = millis();
-  }
 }
 
 // Switch off the light if it is on
